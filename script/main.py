bcd01cda898488b322dca5af2291314544102ac4
obrotoks <oriol@MacBook-Pro-de-Oriol.local>
2023-05-25 17:00:59 +0200

    RDD run

--- script/main.py
+++ script/main.py
@@ -1,11 +1,11 @@
 # New session
 #  cd /usr/bin/spark-3.4.0-bin-hadoop3/bin   
-# ./bin/spark-submit --master spark://spark-master:7077 --conf spark.dynamicAllocation.enable=False --driver-memory g --executorscript/main.py
+# ./bin/spark-submit --master spark://spark-master:7077 script/main.py
 from pyspark.sql import SparkSession
 
 
 spark = SparkSession.builder.getOrCreate()
-df = spark.read.options(header='True',inferschema='True').csv("src/Travel\ Company\ New\ Clients.csv")
+df = spark.read.options(header='True',inferschema='True',delimiter=";").csv("src/Travel\ Company\ New\ Clients.csv")
 df.count()
 df.printSchema()
 df.select("FamilyMembers").distinct().show()
@@ -14,3 +14,7 @@ def myFunc(s):
     return [ ( s["FamilyMembers"],1)]
 
 lines=df.rdd.flatMap(myFunc).reduceByKey(lambda a, b: a + b)
+famColumns =["FamilyMembers","Num_reg"]
+dataColl=lines.toDF(famColumns)
+dataColl.show(truncate=False
+              dataColl.show(truncate=False))
